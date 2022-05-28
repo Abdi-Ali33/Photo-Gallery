a829ca66ec945adcbafc9d89e6c6bd4c87f58bf7
Abdi Ali <alikune33@gmail.com>
2022-05-28 20:53:03 +0300

    creating views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+from app.models import Category, Image, Location
+
 # Create your views here.
+def index(request):
+    images = Image.get_all_images()
+    return render(request, 'index.html', {'images':images, 'title': 'Home'})
+
+def category(request, category_slug):
+    category = Category.get_category_by_slug(category_slug)
+    images = Image.filter_by_category(category)
+    return render(request, 'gallery.html', {'images':images, 'title': category})
+
+def location(request, location_slug):
+    location = Location.get_location_by_slug(location_slug)
+    images = Image.filter_by_location(location)
+    return render(request, 'gallery.html', {'images':images, 'title': location})    
+
+def search(request):
+    q = request.GET['q']
+    images = Image.search_image(q)
+    title = f'Search results for {q}' 
+    return render(request, 'gallery.html', {'images':images, 'title': title})   
+
+
+def handler404(request, exception, template_name="404.html"):
+    context = {}
+    response = render(request, '404.html', context=context)
+    response.status_code = 404
+    return response

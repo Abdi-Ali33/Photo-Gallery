f3aa5cb62c4a1f4cb7b223d667c79c8cd9856f24
Abdi Ali <alikune33@gmail.com>
2022-05-29 18:46:00 +0300

    creating readmefile descript and author info

--- app/admin.py
+++ app/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from .models import Location, Category, Image
 
 
-class LocationAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
+# class LocationAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {"slug": ("name",)}
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
+# class CategoryAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {"slug": ("name",)}
 
 
 # Register your models here.
-admin.site.register(Location, LocationAdmin)
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location)
+admin.site.register(Category)
 admin.site.register(Image)
